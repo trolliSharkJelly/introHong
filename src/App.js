686e3bbe7623c687d34d0ac80a46bb88a60eee65
mimi <hongmikyeong11@gmail.com>
2022-11-01 22:36:16 +0900

    refactor : 페이지 디자인 변경으로 인한 react router 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import Section from "./component/Section";
-import styled, { createGlobalStyle } from "styled-components";
+import About from "./pages/About";
 import Nav from "./component/Nav";
+import Histories from "./pages/Histories";
+import Skills from "./pages/Skills";
+import Portfolioes from "./pages/Portfolioes";
+import { useSelector } from "react-redux";
+import { darkTheme, lightTheme } from "./asset/style/theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./component/About";
-import Skill from "./component/Skill";
-import Portfolioes from "./component/Portfolioes";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 
 export default function App() {
+  const { mode } = useSelector((state) => state.mode);
+
   return (
     <>
       <BrowserRouter>
-        <GlobalStyle />
-        <Main>
-          <Section />
-          <Nav />
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/skill" element={<Skill />} />
-            <Route path="/portfolio" element={<Portfolioes />} />
-          </Routes>
-        </Main>
-        <footer></footer>
+        <ThemeProvider theme={mode ? darkTheme : lightTheme}>
+          <GlobalStyle />
+          <Main>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/history" element={<Histories />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/portfolio" element={<Portfolioes />} />
+            </Routes>
+          </Main>
+        </ThemeProvider>
       </BrowserRouter>
     </>
   );
@@ -31,10 +36,12 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  background-color: ${({ theme }) => theme.background_color};
+  color: ${({ theme }) => theme.color};
 }
 `;
 
 const Main = styled.main`
   display: grid;
-  grid-template-columns: 30% 7% 63%;
+  grid-template-columns: 10% 90%;
 `;
